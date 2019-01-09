954c74c2fbb60b7535b593b231b41855f1212d99
Robert Arthur Gillespie <RobertArthurGillespie@outlook.com>
2019-01-08 22:33:32 -0600

    My first commit

---
+++ MyBlog/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyBlog.Models;
+
+namespace MyBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ViewCustomer(string name, string telephonenumber)
+        {
+            Customer customer = new Customer();
+            customer.ID = Guid.NewGuid().ToString();
+            customer.Name = name;
+            customer.TelephoneNumber = telephonenumber;
+
+            return View(customer);
+        }
+
+        public ActionResult AddCustomer()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyBlog/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyBlog.Models
+{
+    public class Customer
+    {
+        public string ID;
+        public string Name;
+        public string TelephoneNumber;
+    }
+}
